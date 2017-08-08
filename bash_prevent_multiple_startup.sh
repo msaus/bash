11d0a7b03192c62a301f0e53fc143571e415d92b
Masao Soseki <mspipi@msn.com>
2017-08-08 11:55:04 +0900

    add
    a

--- bash_prevent_multiple_startup.sh
+++ bash_prevent_multiple_startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh                                                                                                                                                     
 
+# 引数が無ければメッセージを出力し終了
+if [ "$1" = "" ]; then
+  echo "no argument"
+  exit 1
+fi
+
 # 多重起動防止
 _CMD="$0 $@"
 if [ "$$" -ne $(pgrep -fo "$_CMD") ]; then
